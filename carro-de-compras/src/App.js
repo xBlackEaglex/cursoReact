5906c5de8e7a1af45b6ba67127195830cdc54025
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-03-07 23:49:18 -0600

    se crearon componentes en carro-de-compras

--- carro-de-compras/src/App.js
+++ carro-de-compras/src/App.js
@@ -1,5 +1,9 @@
 import { Component } from 'react'
 import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Title from './components/Title'
+import Navbar from './components/Navbar'
+
 class App extends Component {
   state = {
     productos: [
@@ -23,10 +27,14 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
+        <Layout>
+          <Title />
         <Productos 
           agregarAlCarro = {() => console.log('NO hace nada')}
           productos = {this.state.productos}
         />
+        </Layout>
       </div>
     )
   }
--- carro-de-compras/src/components/Producto.js
+++ carro-de-compras/src/components/Producto.js
@@ -1,14 +1,35 @@
 import { Component } from 'react'
+import Button from './Button'
+
+const styles = {
+    producto: {
+        backgroundColor: '#FFFF',
+        border: '1px solid #eee',
+        boxShadow: '0px 5px 5px rgba(0, 0, 0, 0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px'
+    },
+    img: {
+        width: '100%',
+        height: '240px',
+        objectFit: 'contain'
+    }
+}
+
 
 class Producto extends Component {
     render() {
-        const { producto } = this.props
+        const { producto, agregarAlCarro } = this.props
         return (
             
-            <div>
-                <img alt={producto.name} src={producto.img} />
+            <div style={styles.producto}>
+                <img style={styles.img} alt={producto.name} src={producto.img} />
                 <h3>{producto.name}</h3>
-                <p>{producto.price}</p>
+                <p>$ {producto.price}</p>
+                <Button onClick={() => agregarAlCarro(producto)}>
+                    Agregar al carro
+                </Button>
             </div>
         )
     }
