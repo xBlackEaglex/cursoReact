66dbf2c9755d108723bbbd6c6806fc3ba663173f
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-03-20 19:29:35 -0600

    (interes...) se inicio la clase

---
+++ interes-compuesto/src/App.js
@@ -0,0 +1,36 @@
+import { Formik, Form } from "formik"
+import Input from "./components/Input"
+import Button from "./components/Button"
+import Container from "./components/Container"
+import Section from "./components/Section"
+
+
+function App() {
+  const handleSubmit = () => {}
+  return (
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: '',
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Input name="deposit" label="Deposito inicial" />
+            <Input name="contribution" label="Contribución anual" />
+            <Input name="years" label="Años" />
+            <Input name="rate" label="Interés estimado" />
+            <Button>Calcular</Button>
+          </Form>
+        </Formik>
+      </Section>
+    </Container>
+  )
+}
+
+
+export default App
