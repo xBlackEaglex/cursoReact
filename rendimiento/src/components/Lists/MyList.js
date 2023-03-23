21967580c3b91000c5089a67ad8aa6c43524b4a9
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-03-22 23:39:54 -0600

    (rendimiento...) se termino la clase

---
+++ rendimiento/src/components/Lists/MyList.js
@@ -0,0 +1,56 @@
+import { isEqual } from 'lodash'
+import  { memo } from 'react'
+
+
+// const Li = memo(({ fullname }) => {
+//   console.log(`renderizando ${fullname}`)
+//   return (
+//     <li>
+//       {fullname}
+//     </li>
+//   )
+// }) 
+
+
+// const MyList = ({ data }) => {
+//   console.log('renderizando lista')
+//   return (
+//     <ul>
+//       {data.map(x =>
+//         <Li
+//           key={x.name + x.lastname}
+//           fullname={`${x.name} ${x.lastname}`}
+//         />
+//       )}
+//     </ul>
+//   )
+// }
+
+
+
+const Li = memo(({ children }) => {
+  console.log(`renderizando ${children}`)
+  return (
+    <li>
+      {children}
+    </li>
+  )
+}, isEqual) 
+
+
+const MyList = ({ data }) => {
+  console.log('renderizando lista')
+  return (
+    <ul>
+      {data.map(x =>
+        <Li
+          key={x.name + x.lastname}
+        >
+          {x.name}{x.lastname}
+        </Li>
+      )}
+    </ul>
+  )
+}
+
+export default memo(MyList)
