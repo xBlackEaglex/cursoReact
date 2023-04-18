5232127a3fc1b359f256f77c8f25b2337081d6f1
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-04-17 22:07:45 -0600

    (poke) terminado

--- poke/src/app/pokemones/[id]/page.js
+++ poke/src/app/pokemones/[id]/page.js
@@ -1,7 +1,24 @@
-const Pokemon = () => {
+import Image from "next/image"
+import Link from "next/link"
+
+const getData = async (id) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data = await response.json()
+    return data
+}
+
+const Pokemon = async ({params}) => {
+    const idPoke = params.id;
+    const data =  await getData(idPoke)
+
     return (
-        <p>Lala</p>
+        <div>
+            <h1>{data.name} numero #{data.id}</h1>
+            <Image alt={data.name} src={data.sprites.front_default} width={400} height={400} />
+            <Link href="/">Volver al Inicio</Link>
+        </div>
     );
 }
+
 
 export default Pokemon;
